af3c83ee7ed7346fa40de7d2a6ec8cd423df5817
jeongmin4869 <ljm4135@naver.com>
2023-03-26 20:54:35 +0900

    JavaScript_sort slice

--- Grammer/sort_slice.js
+++ Grammer/sort_slice.js
@@ -19,6 +19,7 @@ array.slice(4)     // [5, 1, 4, 6, 8]
 
 /*  sort  */
 array.sort((a,b)=> {
+    console.log(a, b, a-b);
     return a-b;                  // 오름차순
 });                              // [1, 2, 3, 4, 5, 6, 7, 8, 9]
                                  // compareFunction(a,b) 이 0 보다 작은 경우 a를 b보다 낮은 인덱스로 소트한다. 
@@ -26,12 +27,17 @@ array.sort((a,b)=> {
                                  // sort 는 원본 배열을 변경한다. 
 array.slice().sort((a,b)=>a-b);  // [1, 2, 3, 4, 5, 6, 7, 8, 9]
                                  // slice를 사용하면 원본배열을 변경하지 않고도 정렬할 수 있다. 
-array.slice().sort((a,b)=>a-b);  // [9, 8, 7, 6, 5, 4, 3, 2, 1]
+array.slice().sort((a,b)=>b-a);  // [9, 8, 7, 6, 5, 4, 3, 2, 1]
 
+/* 문자정렬 */
 arr = ['apple', 'peach', 'banana'];
 arr.slice().sort((a,b)=>a-b);                                       // ["apple", "banana", "peach"]
 arr.slice().sort((a,b) => a[0].charCodeAt() - b[0].charCodeAt() );  // ["apple", "banana", "peach"]
                                                                     // 각 문자의 첫번째 넘버의 코드를 비교
 arr.slice().sort((a,b) => a.localCompare(b));                       // ["apple", "banana", "peach"]
                                                                     //  사전 순 정렬
-arr.slice().sort((a,b) => a.localCompare(b));                       // ["peach", "banana", "apple"]
+arr.slice().sort((a,b) => b.localCompare(a));                       // ["peach", "banana", "apple"]
+                                                                    //  사전 순 반대 정렬
+
+
+                                                                    
